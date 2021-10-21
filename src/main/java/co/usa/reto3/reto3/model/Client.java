2e5f033197d7e195fefc5f0c9f2af0cb84abf5b5
joswald123 <48632836+joswald123@users.noreply.github.com>
2021-10-20 21:07:05 -0700

    updated getter & setter for client

--- src/main/java/co/usa/reto3/reto3/model/Client.java
+++ src/main/java/co/usa/reto3/reto3/model/Client.java
@@ -39,47 +39,69 @@ public class Client implements Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	
+//	 Getters & Setters for Client
+
 	public Integer getIdClient() {
 		return idClient;
 	}
+
+
 
 	public void setIdClient(Integer idClient) {
 		this.idClient = idClient;
 	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getEmail() {
 		return email;
 	}
 
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 
 	public Integer getAge() {
 		return age;
 	}
 
+
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	
-//	 Getters & Setters for Client
+
+
 
 	public List<Message> getMessages() {
 		return messages;
